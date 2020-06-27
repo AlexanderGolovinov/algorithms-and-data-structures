5e7ccbd1669cb83d5eeb6c32a6a28701b220e698
AlexanderGolovinov <alexgolovinoff@gmail.com>
2020-06-27 13:12:49 +0300

    Stacks

---
+++ src/algorithms/stacks.js
@@ -0,0 +1,26 @@
+/* Stacks */
+
+//functions: push, pop, peek (show top element), length
+
+let letters = []; //our stack
+
+let word = "racecar"; //is palindrome word
+let word2 = "Alexander"; //is not palindrome word
+let rword = ""; //reversed word
+
+for(let i = 0; i < word.length; i++) {
+    letters.push(word[i]);
+}
+
+for(let i = 0; i < word.length; i++) {
+    rword += letters.pop();
+}
+
+if (word === rword) {
+    console.log(rword + " is palindrome");
+} else {
+    console.log(rword + " is not palindrome");
+
+}
+
+
